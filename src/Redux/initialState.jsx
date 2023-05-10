1c2205d77ef31c2be77b42f4e61902fd778e333f
RitikGajbhiye <invinciblesvibes@gmail.com>
2023-05-10 19:10:41 +0530

    installed packages and did redux setup

---
+++ src/Redux/initialState.jsx
@@ -0,0 +1,5 @@
+export const initialState = {
+  login: {
+    islogged: false,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import RootReducer from "./Redux/RootReducer";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+const store = createStore(RootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
